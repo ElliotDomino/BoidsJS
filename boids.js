c6302bc917a6789664f839eacf3935989af9d68c
Elliot Chrystal <echrysta@ualberta.ca>
2025-04-15 18:35:49 -0600

    Boids Rendering to Canvas!

--- boids.js
+++ boids.js
@@ -1,59 +1,83 @@
+// Roughly following the W3Schools HTML Game Example
 
-var width = 700;
-var height = 500;
+var skyWidth = 600
+var skyHeight = 500
+var intervalTime = 20;
 var numBoids = 50;
 var padding = 20;
-var lineHeight = 15;
+var lineHeight = 15
 
-const boids = new Array();
+const boids = [];
 
-const boidSky = document.getElementById("boidSky");
-const ctx = boidSky.getContext("2d");
-ctx.font = "50px Arial";
-ctx.textBaseline = "middle";
-boidSky.width = width;
-boidSky.height = height;
+var boidSky = {
 
-// init boids
-for (var i = 0; i < numBoids; i++) {
+    canvas : document.getElementById("boidSky"),
 
-    let boid = {
-        id: i,
-        vel_x: 0,
-        vel_y: 0,
-        pos_x: padding + (Math.random() * (width - 2 * padding)),
-        pos_y: padding + (Math.random() * (height - 2 * padding))
+    start : function() {
+
+        this.canvas.width = skyWidth;
+        this.canvas.height = skyHeight;
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateSky, intervalTime);
+        this.context.textAlign = 'center';
+
+    },
+    clear: function() {
+
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    }
+}
+
+class boid {
+    constructor(id, pos_x, pos_y, vel_x, vel_y, angle) {
+
+        this.id = id;
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+        this.vel_x = vel_x;
+        this.vel_y = vel_y;
+        this.angle = angle;
+
+    }
+    update() {
+
+        let ctx = boidSky.context;
+        ctx.save();
+        ctx.translate(this.pos_x, this.pos_y);
+        ctx.rotate((this.angle + 180) * (Math.PI / 180));
+        ctx.fillText("V", 0, lineHeight / 2);
+        ctx.restore();
+
     };
+    
+}
 
-    boids.push(boid)
+function updateSky() {
+
+    console.log(`Clearing Sky!`)
+
+    boidSky.clear();
+    
+    boids.forEach(curBoid => curBoid.update());
 
 }
 
-// draw the boids!
+for (var i = 0; i < numBoids; i++) {
 
-boids.forEach(boid => {
-    
-    ctx.save();
+    var curBoid = new boid(
+        i,
+        padding + (Math.random() * (skyWidth - padding * 2)),
+        padding + (Math.random() * (skyHeight - padding * 2)),
+        0,
+        0,
+        (Math.random() * 360));
 
-    ctx.translate(boid.pos_x, boid.pos_y);
+    boids.push(curBoid);
 
-    let angle = Math.random() * 360;
+}
 
-    ctx.rotate(angle * (Math.PI / 180));
+console.log(boids);
 
-    ctx.textAlign = 'center';
-    ctx.fillText("V", 0, lineHeight / 2);
-
-    ctx.restore();
-
-})
-
-// const boidsDisplay = document.getElementById("boidsDisplay");
-
-// boids.forEach(boid => {
-
-//     const li = document.createElement('li');
-//     li.textContent = `Boid ${boid.id} is at (${Math.round(boid.pos_x)}, ${Math.round(boid.pos_y)}) with velocity (${Math.round(boid.vel_x)}, ${Math.round(boid.vel_y)})`;
-//     boidsDisplay.appendChild(li)
-
-// });
+boidSky.start();
